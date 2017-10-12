02531adbcde6ffa8277d78a7d7ad1dadfd7a6589
Ross Nicoll <ross.nicoll@r3.com>
2017-10-12 15:12:38 +0100

    Replace AbstractPartyToX500NameAsStringConverter with custom type
    Replace AbstractPartyToX500NameAsStringConverter with custom type to stop Hibernate warning about the missing type.
    Downside to this is that it requires manually setting the type on all `AbstractParty` columns.

--- finance/src/main/kotlin/net/corda/finance/schemas/CashSchemaV1.kt
+++ finance/src/main/kotlin/net/corda/finance/schemas/CashSchemaV1.kt
@@ -4,6 +4,7 @@ import net.corda.core.identity.AbstractParty
 import net.corda.core.schemas.MappedSchema
 import net.corda.core.schemas.PersistentState
 import net.corda.core.serialization.CordaSerializable
+import org.hibernate.annotations.Type
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Index
@@ -27,6 +28,7 @@ object CashSchemaV1 : MappedSchema(schemaFamily = CashSchema.javaClass, version 
     class PersistentCashState(
             /** X500Name of owner party **/
             @Column(name = "owner_name")
+            @Type(type = "party")
             var owner: AbstractParty,
 
             @Column(name = "pennies")
